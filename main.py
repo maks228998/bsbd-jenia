49056a8d0a08484c608669ad51ebc9721842ca51
maks228998 <78788414+maks228998@users.noreply.github.com>
2024-06-30 03:22:17 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class LoginWindow(QMainWindow):
     def connect_bd(self, username, password):
         try:
             conn = psycopg2.connect(
-                dbname='jenia',
+                dbname='veterinary_clinic',
                 user=username,
                 password=password,
                 host='localhost'
@@ -32,9 +32,7 @@ class LoginWindow(QMainWindow):
         if conn is None:
             return None
 
-        values = ('lib1',
-                  'rea1',
-                  'adm1')
+        values = ('man1', 'cl1', 'vet1', 'adm1')
 
         cursor = conn.cursor()
         for value in values:
@@ -74,19 +72,19 @@ class lib_gui_Window(QMainWindow):
         self.lib_gui_ui.comboBox.textActivated.connect(self.onActivated)
         self.setCombo()
 
+        self.Button_and_or = ([self.lib_gui_ui.pushButton_and_or_2, self.lib_gui_ui.pushButton_and_or_3,
+                               self.lib_gui_ui.pushButton_and_or_4, self.lib_gui_ui.pushButton_and_or_5,
+                               self.lib_gui_ui.pushButton_and_or_6, self.lib_gui_ui.pushButton_and_or_7,
+                               self.lib_gui_ui.pushButton_and_or_8, self.lib_gui_ui.pushButton_and_or_9])
+
         self.lib_gui_ui.pushButton_and_or_2.clicked.connect(lambda: self.change_button(self.lib_gui_ui.pushButton_and_or_2))
         self.lib_gui_ui.pushButton_and_or_3.clicked.connect(lambda: self.change_button(self.lib_gui_ui.pushButton_and_or_3))
         self.lib_gui_ui.pushButton_and_or_4.clicked.connect(lambda: self.change_button(self.lib_gui_ui.pushButton_and_or_4))
         self.lib_gui_ui.pushButton_and_or_5.clicked.connect(lambda: self.change_button(self.lib_gui_ui.pushButton_and_or_5))
         self.lib_gui_ui.pushButton_and_or_6.clicked.connect(lambda: self.change_button(self.lib_gui_ui.pushButton_and_or_6))
         self.lib_gui_ui.pushButton_and_or_7.clicked.connect(lambda: self.change_button(self.lib_gui_ui.pushButton_and_or_7))
-        self.lib_gui_ui.pushButton_and_or_8.clicked.connect(lambda: self.change_button(self.lib_gui_ui.pushButton_and_or_8))
+        self.lib_gui_ui.pushButton_and_or_8.clicked.connect( lambda: self.change_button(self.lib_gui_ui.pushButton_and_or_8))
         self.lib_gui_ui.pushButton_and_or_9.clicked.connect(lambda: self.change_button(self.lib_gui_ui.pushButton_and_or_9))
-
-        self.Button_and_or = ([self.lib_gui_ui.pushButton_and_or_2, self.lib_gui_ui.pushButton_and_or_3,
-                               self.lib_gui_ui.pushButton_and_or_4, self.lib_gui_ui.pushButton_and_or_5,
-                               self.lib_gui_ui.pushButton_and_or_6, self.lib_gui_ui.pushButton_and_or_7,
-                               self.lib_gui_ui.pushButton_and_or_8, self.lib_gui_ui.pushButton_and_or_9])
 
         self.textEdit = [self.lib_gui_ui.textEdit_1, self.lib_gui_ui.textEdit_2,
                               self.lib_gui_ui.textEdit_3, self.lib_gui_ui.textEdit_4,
@@ -101,14 +99,16 @@ class lib_gui_Window(QMainWindow):
                                  self.lib_gui_ui.textBrowser_7, self.lib_gui_ui.textBrowser_8,
                                  self.lib_gui_ui.textBrowser_9]
 
-        self.lib_gui_ui.pushButton_filter.clicked.connect(self.filter)
-        self.lib_gui_ui.pushButton_add.clicked.connect(self.add)
-        self.lib_gui_ui.pushButton_delete.clicked.connect(self.delete)
-        self.lib_gui_ui.pushButton_update.clicked.connect(self.update)
+        self.lib_gui_ui.pushButton_filter.clicked.connect(self.filter_table)
+        self.lib_gui_ui.pushButton_add.clicked.connect(self.add_table)
+        self.lib_gui_ui.pushButton_delete.clicked.connect(self.delete_table)
+        self.lib_gui_ui.pushButton_update.clicked.connect(self.update_table)
 
     def change_button(self, button):
         if button.text() == "AND":
             button.setText("OR")
+        elif button.text() == "OR":
+            button.setText("")
         else:
             button.setText("AND")
 
@@ -119,7 +119,7 @@ class lib_gui_Window(QMainWindow):
         element.hide()
 
     def setCombo(self):
-        self.lib_gui_ui.comboBox.addItems(["Вид животного", "Порода", "Клиент",
+        self.lib_gui_ui.comboBox.addItems(["Вид животного", "Клиент",
                                           "Работник", "Посещение", "Препарат",
                                           "Питомец", "Должность", "Поставщик",
                                           "Закупка", "Услуга", "Использовано"])
@@ -165,14 +165,12 @@ class lib_gui_Window(QMainWindow):
 
             if self.table in "anim_type":
                 row_labels = ["id", "Вид животного"]
-            elif self.table in "breed":
-                row_labels = ["id", "Порода"]
             elif self.table in "client":
                 row_labels = ["id", "Фамилия", "Имя", "Отчество", "Адресс", "Телефон", "Электронная почта", "Дополнительная информация"]
             elif self.table in "employee":
                 row_labels = ["id", "Фамилия", "Имя", "Отчество", "Телефон", "Электронная почта", "Должность","Дополнительная информация", "Дата рождения"]
             elif self.table in "log_of_visits":
-                row_labels = ["id", "Дата", "Клиент", "Ветеринар", "Услуга", "Животное", "Было использовано", "Время приема", "Статус приема"]
+                row_labels = ["id", "Дата", "Животное", "Ветеринар", "Услуга", "Было использовано", "Время приема", "Статус приема"]
             elif self.table in "medicine":
                 row_labels = ["id", "Наименования", "Описание"]
             elif self.table in "pet":
@@ -187,6 +185,9 @@ class lib_gui_Window(QMainWindow):
                 row_labels = ["id", "Наименование", "Стоимость"]
             elif self.table in "the_medicine_used":
                 row_labels = ["id", "Визит", "Колличество", "Наименование"]
+
+            for i in range(len(row_labels)):
+                self.textBrowser[i].setText(row_labels[i])
 
             self.lib_gui_ui.tableWidget.setColumnCount(len(colum_name))
             self.lib_gui_ui.tableWidget.setHorizontalHeaderLabels(row_labels)
@@ -206,11 +207,15 @@ class lib_gui_Window(QMainWindow):
             for i in range(len(colum_name)):
                 self.do_show(self.textBrowser[i])
 
-            print('len(colum_name)', len(colum_name))
-            print('colum_name', colum_name)
-
             cursor = self.conn.cursor()
-            cursor.execute(f"SELECT * FROM anim_type")
+            cursor.execute(f"SAVEPOINT SP1")
+
+            cursor.execute(f"select * from current_user")
+            username = cursor.fetchall()
+            if  str(username)[3:5] == 'cl' and (self.table == 'log_of_visits' or self.table == 'client' or self.table == 'pet'):
+                cursor.execute(f"SELECT * FROM " + self.table + " where client_id = '" + str(username)[5:-4] + "'")
+            else:
+                cursor.execute(f"SELECT * FROM " + self.table)
             bib = cursor.fetchall()
 
             self.lib_gui_ui.tableWidget.setRowCount(len(bib))
@@ -219,10 +224,14 @@ class lib_gui_Window(QMainWindow):
                     self.lib_gui_ui.tableWidget.setItem(i, j, QTableWidgetItem(str(bib[i][j])))
 
             self.lib_gui_ui.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+
+            cursor.execute(f"RELEASE SAVEPOINT SP1")
+
         except(Exception, Error) as error:
             QMessageBox.warning(self, 'Ошибка!', str(error))
+            cursor.execute(f"ROLLBACK TO SAVEPOINT SP1")
 
-    def filter(self):
+    def filter_table(self):
         try:
             cursor = self.conn.cursor()
             sql_colum_name = (
@@ -231,52 +240,14 @@ class lib_gui_Window(QMainWindow):
             colum_name = cursor.fetchall()
 
             sql = (f"SELECT * FROM " + self.table + " WHERE (")
-
-            if self.lib_gui_ui.textEdit_1.toPlainText():
-                sql = sql + str(colum_name[0])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_1.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_2.toPlainText():
-                if self.lib_gui_ui.textEdit_1.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_2.text()) + " "
-                sql = sql + str(colum_name[1])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_2.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_3.toPlainText():
-                if self.lib_gui_ui.textEdit_2.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_3.text()) + " "
-                sql = sql + str(colum_name[2])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_3.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_4.toPlainText():
-                if self.lib_gui_ui.textEdit_3.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_4.text()) + " "
-                sql = sql + str(colum_name[3])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_4.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_5.toPlainText():
-                if self.lib_gui_ui.textEdit_4.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_5.text()) + " "
-                sql = sql + str(colum_name[4])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_5.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_6.toPlainText():
-                if self.lib_gui_ui.textEdit_5.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_6.text()) + " "
-                sql = sql + str(colum_name[5])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_6.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_7.toPlainText():
-                if self.lib_gui_ui.textEdit_6.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_7.text()) + " "
-                sql = sql + str(colum_name[6])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_7.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_8.toPlainText():
-                if self.lib_gui_ui.textEdit_7.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_8.text()) + " "
-                sql = sql + str(colum_name[7])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_8.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_9.toPlainText():
-                if self.lib_gui_ui.textEdit_8.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_9.text()) + " "
-                sql = sql + str(colum_name[8])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_9.toPlainText()) + "'"
-
+            for i in range(len(colum_name)):
+                if self.textEdit[i].toPlainText():
+                    if self.Button_and_or[i - 1].text() != '':
+                        sql = sql + " " + self.Button_and_or[i - 1].text() + " "
+                    sql = sql + colum_name[i][0] + " = '" + str(self.textEdit[i].toPlainText()) + "'"
             sql = sql + ");"
 
+            cursor.execute(f"SAVEPOINT SP1")
             cursor.execute(sql)
             filter_result = cursor.fetchall()
 
@@ -286,10 +257,13 @@ class lib_gui_Window(QMainWindow):
                     self.lib_gui_ui.tableWidget.setItem(i, j, QTableWidgetItem(str(filter_result[i][j])))
             self.lib_gui_ui.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
 
+            cursor.execute(f"RELEASE SAVEPOINT SP1")
+
         except(Exception, Error) as error:
             QMessageBox.warning(self, 'Ошибка!', str(error))
+            cursor.execute(f"ROLLBACK TO SAVEPOINT SP1")
 
-    def add(self):
+    def add_table(self):
         try:
             cursor = self.conn.cursor()
             sql = (f"INSERT INTO  " + self.table + " VALUES ('")
@@ -297,31 +271,35 @@ class lib_gui_Window(QMainWindow):
             if self.lib_gui_ui.textEdit_1.toPlainText():
                 sql = sql + self.lib_gui_ui.textEdit_1.toPlainText()
             if self.lib_gui_ui.textEdit_2.toPlainText():
-                sql = sql  + "', '" + self.lib_gui_ui.textEdit_2.toPlainText()
+                sql = sql + "', '" + self.lib_gui_ui.textEdit_2.toPlainText()
             if self.lib_gui_ui.textEdit_3.isVisible() and self.lib_gui_ui.textEdit_3.toPlainText():
-                sql = sql  + "', '" + self.lib_gui_ui.textEdit_3.toPlainText()
+                sql = sql + "', '" + self.lib_gui_ui.textEdit_3.toPlainText()
             if self.lib_gui_ui.textEdit_4.isVisible() and self.lib_gui_ui.textEdit_4.toPlainText():
-                sql = sql  + "', '" + self.lib_gui_ui.textEdit_4.toPlainText()
+                sql = sql + "', '" + self.lib_gui_ui.textEdit_4.toPlainText()
             if self.lib_gui_ui.textEdit_5.isVisible() and self.lib_gui_ui.textEdit_5.toPlainText():
-                sql = sql  + "', '" + self.lib_gui_ui.textEdit_5.toPlainText()
+                sql = sql + "', '" + self.lib_gui_ui.textEdit_5.toPlainText()
             if self.lib_gui_ui.textEdit_6.isVisible() and self.lib_gui_ui.textEdit_6.toPlainText():
-                sql = sql  + "', '" + self.lib_gui_ui.textEdit_6.toPlainText()
+                sql = sql + "', '" + self.lib_gui_ui.textEdit_6.toPlainText()
             if self.lib_gui_ui.textEdit_7.isVisible() and self.lib_gui_ui.textEdit_7.toPlainText():
-                sql = sql  + "', '" + self.lib_gui_ui.textEdit_7.toPlainText()
+                sql = sql + "', '" + self.lib_gui_ui.textEdit_7.toPlainText()
             if self.lib_gui_ui.textEdit_8.isVisible() and self.lib_gui_ui.textEdit_8.toPlainText():
-                sql = sql  + "', '" + self.lib_gui_ui.textEdit_8.toPlainText()
+                sql = sql + "', '" + self.lib_gui_ui.textEdit_8.toPlainText()
             if self.lib_gui_ui.textEdit_9.isVisible() and self.lib_gui_ui.textEdit_9.toPlainText():
-                sql = sql  + "', '" + self.lib_gui_ui.textEdit_9.toPlainText()
+                sql = sql + "', '" + self.lib_gui_ui.textEdit_9.toPlainText()
 
             sql = sql + "');"
 
+            cursor.execute(f"SAVEPOINT SP1")
             cursor.execute(sql)
             self.conn.commit()
 
+            cursor.execute(f"RELEASE SAVEPOINT SP1")
+
         except(Exception, Error) as error:
             QMessageBox.warning(self, 'Ошибка!', str(error))
+            cursor.execute(f"ROLLBACK TO SAVEPOINT SP1")
 
-    def delete(self):
+    def delete_table(self):
         try:
             cursor = self.conn.cursor()
             sql_colum_name = (
@@ -330,58 +308,23 @@ class lib_gui_Window(QMainWindow):
             colum_name = cursor.fetchall()
 
             sql = (f"DELETE FROM " + self.table + " WHERE (")
-
-            if self.lib_gui_ui.textEdit_1.toPlainText():
-                sql = sql + str(colum_name[0])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_1.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_2.toPlainText():
-                if self.lib_gui_ui.textEdit_1.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_2.text()) + " "
-                sql = sql + str(colum_name[1])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_2.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_3.toPlainText():
-                if self.lib_gui_ui.textEdit_2.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_3.text()) + " "
-                sql = sql + str(colum_name[2])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_3.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_4.toPlainText():
-                if self.lib_gui_ui.textEdit_3.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_4.text()) + " "
-                sql = sql + str(colum_name[3])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_4.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_5.toPlainText():
-                if self.lib_gui_ui.textEdit_4.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_5.text()) + " "
-                sql = sql + str(colum_name[4])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_5.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_6.toPlainText():
-                if self.lib_gui_ui.textEdit_5.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_6.text()) + " "
-                sql = sql + str(colum_name[5])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_6.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_7.toPlainText():
-                if self.lib_gui_ui.textEdit_6.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_7.text()) + " "
-                sql = sql + str(colum_name[6])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_7.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_8.toPlainText():
-                if self.lib_gui_ui.textEdit_7.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_8.text()) + " "
-                sql = sql + str(colum_name[7])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_8.toPlainText()) + "'"
-
-            if self.lib_gui_ui.textEdit_9.toPlainText():
-                if self.lib_gui_ui.textEdit_8.toPlainText():
-                    sql = sql + " " + str(self.lib_gui_ui.pushButton_and_or_9.text()) + " "
-                sql = sql + str(colum_name[8])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_9.toPlainText()) + "'"
-
+            for i in range(len(colum_name)):
+                if self.textEdit[i].toPlainText():
+                    if self.Button_and_or[i - 1].text() != '':
+                        sql = sql + " " + self.Button_and_or[i - 1].text() + " "
+                    sql = sql + colum_name[i][0] + " = '" + str(self.textEdit[i].toPlainText()) + "'"
             sql = sql + ");"
 
+            cursor.execute(f"SAVEPOINT SP1")
             cursor.execute(sql)
+
+            cursor.execute(f"RELEASE SAVEPOINT SP1")
 
         except(Exception, Error) as error:
             QMessageBox.warning(self, 'Ошибка!', str(error))
+            cursor.execute(f"ROLLBACK TO SAVEPOINT SP1")
 
-    def update(self):
+    def update_table(self):
         try:
             cursor = self.conn.cursor()
             sql_colum_name = (
@@ -434,12 +377,17 @@ class lib_gui_Window(QMainWindow):
                     sql = sql + ", "
                 sql = sql + str(colum_name[8])[2:-3] + " = '" + str(self.lib_gui_ui.textEdit_9.toPlainText()) + "'"
 
-            sql = sql + " WHERE (" + str(colum_name[0])[2:-3] + " = '" + (self.lib_gui_ui.tableWidget.item(self.lib_gui_ui.tableWidget.currentRow(), 0).text()) + "');"
+            sql = sql + " WHERE (" + str(colum_name[0])[2:-3] + " = '" + (
+                self.lib_gui_ui.tableWidget.item(self.lib_gui_ui.tableWidget.currentRow(), 0).text()) + "');"
 
+            cursor.execute(f"SAVEPOINT SP1")
             cursor.execute(sql)
+
+            cursor.execute(f"RELEASE SAVEPOINT SP1")
 
         except(Exception, Error) as error:
             QMessageBox.warning(self, 'Ошибка!', str(error))
+            cursor.execute(f"ROLLBACK TO SAVEPOINT SP1")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
